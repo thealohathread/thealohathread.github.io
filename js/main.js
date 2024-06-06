b266f3bd6c750e32adc2a08a4a5a49d1f1ffd49b
thealohathread <155278802+thealohathread@users.noreply.github.com>
2024-06-06 08:00:43 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -314,7 +314,7 @@
 // timer countdown
 	function makeTimer() {
 
-		var endTime = new Date("31 May 2024 23:59:00 GMT-0500");			
+		var endTime = new Date("14 June 2024 23:59:00 GMT-0500");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
